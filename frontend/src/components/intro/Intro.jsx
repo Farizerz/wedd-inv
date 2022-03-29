d19c663d0112baa329e171fa9eee7e764d909ed2
Farizerz <39519098+Farizerz@users.noreply.github.com>
2022-03-29 21:40:27 +0700

    from home PC

--- frontend/src/components/intro/Intro.jsx
+++ frontend/src/components/intro/Intro.jsx
@@ -5,8 +5,8 @@ const Intro = () => {
   return (
     <div className='intro'>
         <div className='intro-bg'>
-            <img className='intro-bg-img' src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/257582892_5179769602037905_7958927071553477875_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=eUqIRnwBx44AX_9Gq2y&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Pgg4DQtEnZRV7-eNAC7ruzD-OzX5jjeZ9Y6JQR6JhXQ&oe=6247D171" />
-            <div className='intro-bg-opacity'></div>
+        <img className='intro-bg-img' src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/257582892_5179769602037905_7958927071553477875_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=eUqIRnwBx44AX_9Gq2y&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Pgg4DQtEnZRV7-eNAC7ruzD-OzX5jjeZ9Y6JQR6JhXQ&oe=6247D171" />
+            <div className='intro-bg-overlay'></div>
         </div>
         <div className='intro-title'>
             <p className='intro-title-subject'>We Are Getting Married</p>
--- frontend/src/components/quotes/Quotes.jsx
+++ frontend/src/components/quotes/Quotes.jsx
@@ -1,13 +1,20 @@
 import './quotes.css'
 import React from 'react'
 import Bunga from '../../img/flower-bouquet.png'
+import GambarSatu from '../../img/1.png';
 
 const Quotes = () => {
   return (
     <div className='quotes'>
-        <div className='quotes-bg'></div>
-        <div className='quotes-wrapper'>
-            <img src={Bunga} className='quotes-flower' />
+        <div className='quotes-bg'>
+          <img src={Bunga} className='quotes-flower' />
+          <div className='quotes-wrapper'>
+              <p className='quotes-prayer'>“And of His signs is that He created for you from yourselves mates that you may find tranquillity in them; and He placed between you affection and mercy. Indeed in that are signs for a people who give thought.”</p>
+              <p className='quotes-verse'>( QS. Ar- Rum 21 )</p>           
+          </div>
+          <div className='quotes-satu'>
+            <img src={GambarSatu} className='quotes-gambarSatu' />
+          </div>
         </div>
     </div>
   )
