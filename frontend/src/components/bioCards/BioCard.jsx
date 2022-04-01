b39748fecb3a45db32d897d5d1170d83df50355e
Farizerz <39519098+Farizerz@users.noreply.github.com>
2022-04-01 23:54:04 +0700

    building bioCard

--- frontend/src/components/bioCards/BioCard.jsx
+++ frontend/src/components/bioCards/BioCard.jsx
@@ -1,28 +1,34 @@
 import React from 'react'
 import './bioCard.css';
 import PP from '../../img/PP1-254.png';
+import { FaInstagram as Instagram } from 'react-icons/fa'
 
 const BioCard = () => {
   return (
     <div className='bioCard'>
         
         <div className='bioCard-container'>
-            
-
+            {/* isi container */}
             <div className='bioCard-contents'>
                 <img className='bioCard-img' src={PP} />
                 <p className='bioCard-name'>Denny Ammari Ramadhan Nasution, S.E</p>
+                <p className='bioCard-childOf'>Putra Pertama dari</p>
+                <p className='bioCard-parents'>Bpk. Ade Parlaungan Nasution, S.E., M.Si., Ph.D. <br /> & Ibu Eva Dewi Melinda Nasution, A.Md. Keb (Almh)</p>
+                <form action='https://www.instagram.com/dennyammary/' target='_blank'>
+                  <button type='submit'>
+                    <Instagram />
+                  </button>
+                </form>
             </div>
-            
-            
         </div>
 
+        {/* Accessories */}
         <div className='bioCard-container2'>
             <div className='bioCard-leftAcc'></div>
             <div className='bioCard-rightAcc'></div>
         </div>
 
-        {/* left Accessories */}
+        
         
     </div>
   )
