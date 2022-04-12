406710a6e6b2c084c6e67d0caf5ba453cdf88c46
Farizerz <39519098+Farizerz@users.noreply.github.com>
2022-04-12 21:46:04 +0700

    editing from home pc

--- frontend/src/components/intro/Intro.jsx
+++ frontend/src/components/intro/Intro.jsx
@@ -8,11 +8,12 @@ const Intro = () => {
         <div className='intro-bg'>
           <img className='intro-bg-img' src={DnnyAjng} />
           <div className='intro-bg-overlay'></div>
-        </div>
-        <div className='intro-title'>
-            <p className='intro-title-subject'>We Are Getting Married</p>
-            <h1 className='intro-title-name'>Ajeng & Denny</h1>
-            <p className='intro-title-date'>May 28, 2022</p>
+        
+          <div className='intro-title'>
+              <p className='intro-title-subject'>We Are Getting Married</p>
+              <h1 className='intro-title-name'>Ajeng & Denny</h1>
+              <p className='intro-title-date'>May 28, 2022</p>
+          </div>
         </div>
     </div>
   )
